0b058af06394aea1bc955f291c7f6d494af608f3
Scott Langridge <scott.m.langridge@gmail.com>
2020-02-01 16:24:11 +0000

    Lots of small changes while running with a match on

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ match = RugbyMatch('EVP3013155')
 display = DisplayController()
 
 team_names = [team['displayName'] for team in match.team_stats]
-
+scoreline = ""
 while True:
     scores = [str(team['runningScore']) for team in match.team_stats]
     scores[0] = scores[0].rjust(2, ' ')
     scores[1] = scores[1].ljust(2, ' ')
+
+    last_scoreline = scoreline
     scoreline = '%s %s - %s %s' % (team_names[0], scores[0], scores[1], team_names[1])
-    display.display_txt(scoreline)
+    if scoreline != last_scoreline:
+        display.display_txt(scoreline)
 
     sleep(30)
-    match.update()
+    try:
+       match.update()
+    except:
+        print("EXCEPTION!")
+
